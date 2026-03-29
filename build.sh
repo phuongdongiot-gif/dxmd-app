214bc908ae048cfc33573ace91017dd7aa46257e
Phuong Dong <catmusic435@gmail.com>
2026-03-29 22:34:38 +0700

    perf: optimize Flutter SDK installation by using a shallow git clone to reduce build memory usage

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script tự động tải Flutter SDK và Build Web trên nền tảng Vercel
 
-echo "Installing Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable
+echo "Installing Flutter (Shallow Clone)..."
+# Sử dụng --depth 1 để tải bản nhẹ nhất và nhanh nhất, giúp Vercel không bị lỗi đầy bộ nhớ
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Checking Flutter version..."
