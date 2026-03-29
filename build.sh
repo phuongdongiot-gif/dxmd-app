682334157aec911a543f99aadb5d45775c92c931
Phuong Dong <catmusic435@gmail.com>
2026-03-29 22:36:33 +0700

    chore: update build script to output to public directory for Vercel compatibility

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Script tự động tải Flutter SDK và Build Web trên nền tảng Vercel
+set -e # Báo lỗi và dừng script ngay nếu có 1 lệnh thất bại
 
 echo "Installing Flutter (Shallow Clone)..."
-# Sử dụng --depth 1 để tải bản nhẹ nhất và nhanh nhất, giúp Vercel không bị lỗi đầy bộ nhớ
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
 echo "Checking Flutter version..."
 flutter --version
@@ -12,4 +12,8 @@ flutter --version
 echo "Building Flutter Web..."
 flutter build web --release --web-renderer canvaskit
 
-echo "Build complete! Vercel will serve the build/web directory."
+echo "Organizing Output Directory for Vercel..."
+rm -rf public
+cp -r build/web public
+
+echo "Build complete! Vercel will serve the 'public' directory."
